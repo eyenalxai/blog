9ca6fa7be46ae2997633675fe6004a898e0f4014
Sax Dhiut <mail@iyxeyl.me>
2023-07-18 03:35:15 +0400

    misc: Update README.md and meta

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,15 @@ import { Posts } from '@/components/posts'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'A Blog',
-  description: 'Just a blog',
+  title:
+    'A blog personal, blogal person made, blog person madea, made blog persona',
+  description:
+    'I made a personal blog, a blog made personally, person made blogal, the blog person madi, made person logiba, bloga lipe adomrans, roblagon amadipes, blogs maidenal persons, blogaperson maideni, emablog radin poesla, dograb minalpseo, erblog madio sanpel, bloger mandiosa elp, lediblog namosap, persona diblog ameil, bermado blogasin peol, admani blogerosp le, logas pinomed breal, personm adlogai ble, pblog aodien rmsal, personal blogi mande.',
   openGraph: {
-    title: 'A Blog',
-    description: 'Just a blog'
+    title:
+      'A blog personal, blogal person made, blog person madea, made blog persona',
+    description:
+      'I made a personal blog, a blog made personally, person made blogal, the blog person madi, made person logiba, bloga lipe adomrans, roblagon amadipes, blogs maidenal persons, blogaperson maideni, emablog radin poesla, dograb minalpseo, erblog madio sanpel, bloger mandiosa elp, lediblog namosap, persona diblog ameil, bermado blogasin peol, admani blogerosp le, logas pinomed breal, personm adlogai ble, pblog aodien rmsal, personal blogi mande.'
   }
 }
 
