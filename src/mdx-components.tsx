a9de1108057771ea6e8bfe159a8b26c170584999
Sax Dhiut <mail@iyxeyl.me>
2024-10-16 19:48:28 +0400

    feat: add custom code block component to MDX
    Integrated a custom code block component to enhance MDX processing. This change allows for inline code and language-specific code blocks to be rendered using the new MemoizedCodeComponent, improving the readability and functionality of code examples in MDX files.

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,20 @@
+import { MemoizedCodeComponent } from "@/components/code-block"
+import { isInline } from "@/lib/utils"
 import type { MDXComponents } from "mdx/types"
+
+const customComponents: MDXComponents = {
+	code: (props) => {
+		if (isInline(props.children)) return <code {...props} />
+
+		const match = /language-(\w+)/.exec(props.className || "")
+		if (!match) return <code {...props} />
+		return <MemoizedCodeComponent key={Math.random()} {...props} language={match ? match[1] : ""} />
+	}
+}
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
-		...components
+		...components,
+		...customComponents
 	}
 }
